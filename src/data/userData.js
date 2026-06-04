03188a6ab1ca79be89e6b3032cf50db356cd65a6
Lautaro Nola <lautaronola@gmail.com>
2026-06-03 22:48:00 -0300

    data connection and data userData, constants, login and register user

---
+++ src/data/userData.js
@@ -0,0 +1,46 @@
+import { getDb } from "./connection";
+import { ObjectId } from "mongodb";
+import bcrypt from "bcrypt";
+import {
+  USER_ALREADY_EXISTS,
+  USER_NOT_FOUND_ERROR,
+  LOGIN_FAILED_ERROR,
+} from "../constants";
+
+export async function registerUser(name, email, password) {
+  const db = await getDb();
+
+  const existingUser = await db.collection("users").findOne({ email });
+  if (existingUser) {
+    throw new Error(USER_ALREADY_EXISTS);
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = await db
+    .collection("users")
+    .insertOne({ email, password: hashedPassword, name });
+  return newUser;
+}
+
+export async function loginUser(email, password) {
+  const db = await getDb();
+
+  const user = await db.collection("users").findOne({ email });
+  if (!user) {
+    throw new Error(LOGIN_FAILED_ERROR);
+  }
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    throw new Error(LOGIN_FAILED_ERROR);
+  }
+  return user;
+}
+
+export async function getUserById(id) {
+  const db = await getDb();
+  const user = await db.collection("users").findOne({ _id: new ObjectId(id) });
+  if (!user) {
+    throw new Error(USER_NOT_FOUND_ERROR);
+  }
+  return user;
+}
