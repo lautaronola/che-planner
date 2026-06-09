531b8db7e47134ec904673be1437b266d8072680
Sabrina <laxsabrina1@gmail.com>
2026-06-09 13:11:19 -0300

    feat: desarrollo de paymentData.js y metodos: createpayment, getPaymentsByTrip, settlePayment

---
+++ src/data/paymentsData.js
@@ -0,0 +1,32 @@
+import { getDb } from "./connection.js";
+import { ObjectId } from "mongodb";
+
+export async function createPayment(tripId, payerId, amount, description, splitBetween) {
+  const db = await getDb();
+  const newPayment = {
+    tripId: new ObjectId(tripId),
+    payerId: new ObjectId(payerId),
+    amount,
+    description,
+    splitBetween: splitBetween.map((id) => new ObjectId(id)),
+    settled: false,
+    createdAt: new Date(),
+  };
+  const result = await db.collection("payments").insertOne(newPayment);
+  return { _id: result.insertedId, ...newPayment };
+}
+
+export async function getPaymentsByTrip(tripId) {
+  const db = await getDb();
+  return db.collection("payments").find({ tripId: new ObjectId(tripId) }).toArray();
+}
+
+// Marcar pago saldado
+
+export async function settlePayment(paymentId) {
+  const db = await getDb();
+  return db.collection("payments").updateOne(
+    { _id: new ObjectId(paymentId) },
+    { $set: { settled: true } } 
+  );
+}
