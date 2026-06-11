c264f4ad45a7a4cdf575b03723e50d52bcda13f0
Sabrina <laxsabrina1@gmail.com>
2026-06-11 15:07:47 -0300

    fix: add .js extension to imports in userData.js

--- src/data/userData.js
+++ src/data/userData.js
@@ -1,11 +1,11 @@
-import { getDb } from "./connection";
+import { getDb } from "./connection.js";
 import { ObjectId } from "mongodb";
 import bcrypt from "bcrypt";
 import {
   USER_ALREADY_EXISTS,
   USER_NOT_FOUND_ERROR,
   LOGIN_FAILED_ERROR,
-} from "../constants";
+} from "../constants/index.js";
 
 export async function registerUser(name, email, password) {
   const db = await getDb();
